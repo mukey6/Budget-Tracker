690e9a8d9ec17c8be1323639347e6b148eeaad24
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-02-24 23:40:07 -0600

    updated service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,32 @@
-const APP_PREFIX = 'budgetTracker-';     
+const APP_PREFIX = 'budgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
-// add cache files 
-'./index.html',
-'./public/js/index.js',
-'/public/css/styles.css',
-'/public/js/idb.js',
-'../server.js'
-
+  // add cache files 
+  "/",
+  "./index.html",
+  "./css/styles.css",
+  "./js/idb.js",
+  "./js/index.js",
+  "./manifest.json",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
 ];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
@@ -30,17 +47,6 @@ self.addEventListener('fetch', function (e) {
   )
 })
 
-// Cache resources
-
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
-    })
-  )
-})
-
 
 // Delete outdated caches
 self.addEventListener('activate', function (e) {
@@ -56,10 +62,11 @@ self.addEventListener('activate', function (e) {
 
       return Promise.all(keyList.map(function (key, i) {
         if (cacheKeeplist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] );
+          console.log('deleting cache : ' + keyList[i]);
           return caches.delete(keyList[i]);
         }
       }));
     })
   );
 });
+
